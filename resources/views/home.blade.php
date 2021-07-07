f39937d80e98630b4579cd273cc5233398afbd7b
Cargalher <cargalher@gmail.com>
2021-07-07 17:50:24 +0200

    added style to menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #ffc0cb30;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
@@ -47,19 +47,13 @@
             .title {
                 font-size: 84px;
             }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
             .m-b-md {
                 margin-bottom: 30px;
+            }
+            .menu:hover{
+                font-weight: 700;
+                cursor: pointer;
+                border-bottom: 1px solid #636b6f;
             }
         </style>
     </head>
@@ -86,7 +80,7 @@
 
                 @foreach($details as $detail)
 
-                    <span>{{$detail}}</span>
+                    <span class="menu">{{$detail}}</span>
                     @if(!$loop->last)
                     ※
                     @endif
